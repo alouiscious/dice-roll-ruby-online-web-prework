24bbb4cb35324cc07d7a1094441b2f8adcf29a93
Alouiscious Sims <info@reneesoul.com>
2019-06-08 18:32:48 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -6,12 +6,16 @@ def roll(num = rand(1..6))
   num
 end
 
-num
+puts roll
 
 def roll_rand(num = range(1..6))
   num.rand
 end
 
+puts roll_rand
+
 def roll_array(num = Array.new(rand(1..6)))
   num(0)
 end
+
+puts roll_array
